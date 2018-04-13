87d2a994e33832aa5ab689df94aae90b93d33077
yellowfrog <174700701@qq.com>
2018-04-13 17:07:50 +0800

    fix: 修复actionCreator的info检验警告提示

--- lib/actionCreator/index.ts
+++ lib/actionCreator/index.ts
@@ -7,7 +7,7 @@ function actionCreator (type: string = '', info: string = '') : Object {
         console.error('使用actionCreator创建action的时候,第一个参数是 action.type,请务必输入字符串,否则这个函数没有意义');
         return { type: '' };
     }
-    if (info || typeof(info) !== 'string') {
+    if (!info || typeof(info) !== 'string') {
         console.warn('最好在使用actionCreator创建action的时候,加入第二个参数,且是String类型,为了更方便您日后的工作！');
     }
     return function (payload: any, extendType : string = '') : Object {
